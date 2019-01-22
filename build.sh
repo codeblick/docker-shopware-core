42126360ad6dd34557a44e591ed660ccec482f96
Johannes Berthel <j.berthel@codeblick.de>
2019-01-22 09:58:38 +0100

    added newer and older docker core versions

--- build.sh
+++ build.sh
@@ -9,6 +9,15 @@ build() {
         -q
 }
 
+build 5.6 5.3.0
+build 5.6 5.3.1
+build 5.6 5.3.2
+build 5.6 5.3.3
+build 5.6 5.3.4
+build 5.6 5.3.5
+build 5.6 5.3.6
+build 5.6 5.3.7
+
 build 7.0 5.4.0
 build 7.0 5.4.1
 build 7.0 5.4.2
@@ -22,3 +31,4 @@ build 7.2 5.5.1
 build 7.2 5.5.2
 build 7.2 5.5.3
 build 7.2 5.5.4
+build 7.2 5.5.5
--- push.sh
+++ push.sh
@@ -7,6 +7,15 @@ push() {
 
 docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
 
+push 5.3.0
+push 5.3.1
+push 5.3.2
+push 5.3.3
+push 5.3.4
+push 5.3.5
+push 5.3.6
+push 5.3.7
+
 push 5.4.0
 push 5.4.1
 push 5.4.2
@@ -20,3 +29,4 @@ push 5.5.1
 push 5.5.2
 push 5.5.3
 push 5.5.4
+push 5.5.5
