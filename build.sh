dfe6db1787f6d90d90eeb751b1fbeca05905ad70
j.berthel <j.berthel@codeblick.de>
2019-08-27 15:48:51 +0200

    added newer shopware versions, removed old ones

--- build.sh
+++ build.sh
@@ -9,15 +9,6 @@ build() {
         -q
 }
 
-build 5.6 5.3.0
-build 5.6 5.3.1
-build 5.6 5.3.2
-build 5.6 5.3.3
-build 5.6 5.3.4
-build 5.6 5.3.5
-build 5.6 5.3.6
-build 5.6 5.3.7
-
 build 7.0 5.4.0
 build 7.0 5.4.1
 build 7.0 5.4.2
@@ -35,3 +26,7 @@ build 7.2 5.5.5
 build 7.2 5.5.6
 build 7.2 5.5.7
 build 7.2 5.5.8
+build 7.2 5.5.9
+build 7.2 5.5.10
+
+build 7.2 5.6.0
