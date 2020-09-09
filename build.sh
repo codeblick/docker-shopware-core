4c2acde7ea30d36b7b41755ce7daab032371d1c6
Johannes Berthel <j.berthel@codeblick.de>
2020-09-09 11:28:28 +0200

    remove sw version 5.6.5 because it does not exist

--- build.sh
+++ build.sh
@@ -5,16 +5,14 @@ build() {
     docker build . \
         -t codeblick/shopware-core:ce-${2} \
         --build-arg COB_PHP_VERSION=${1} \
-        --build-arg COB_SW_VERSION=${2} \
-        -q
+        --build-arg COB_SW_VERSION=${2}
 }
 
-build 7.3 5.6.0
-build 7.3 5.6.1
-build 7.3 5.6.2
-build 7.3 5.6.3
-build 7.3 5.6.4
-build 7.3 5.6.5
+# build 7.3 5.6.0
+# build 7.3 5.6.1
+# build 7.3 5.6.2
+# build 7.3 5.6.3
+# build 7.3 5.6.4
 build 7.3 5.6.6
-build 7.3 5.6.7
-build 7.3 5.6.8
+# build 7.3 5.6.7
+# build 7.3 5.6.8
