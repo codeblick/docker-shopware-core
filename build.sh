ad51710b1a10f80fe1a0ec2b0d028ca7879a6c89
Johannes Berthel <mail@johannesberthel.de>
2022-01-10 15:19:54 +0100

    updated shopware version

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ build() {
         --build-arg COB_SW_VERSION=${2}
 }
 
-build 7.3 5.6.10
-build 7.4 5.7.0
-build 7.4 5.7.2
+build 7.4 5.7.3
+build 7.4 5.7.4
+build 7.4 5.7.5
+build 7.4 5.7.6
+build 7.4 5.7.7
